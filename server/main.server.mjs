e606284feba9171d2f5cd41fe874c665896f17cb
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2025-05-16 22:40:35 +0700

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WRM6JTN2.mjs";import"./chunk-AVOB6QKR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HLBVTVXJ.mjs";import"./chunk-AVOB6QKR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
